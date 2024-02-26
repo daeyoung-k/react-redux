afc327b3ea49b981f100fd93feff8c20132a2926
kkwon_dae <eodud4976@gmail.com>
2024-02-26 10:55:18 +0900

    redux

---
+++ src/components/DisplayNumber.tsx
@@ -0,0 +1,10 @@
+const DisplayNumber = () => {
+    return (
+        <div>
+            <h1>Display Number</h1>
+            <input type="button" value="0" readOnly></input>
+        </div>
+    );
+};
+
+export default DisplayNumber;
